bf5ea8a8313f40bc32babecfc51a5193f847de54
E. Andrew Eccleston <eandreweccleston@Es-MacBook-Pro.local>
2016-04-08 11:15:49 -0500

    working

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -8,23 +8,33 @@ function formSweep (){
             formData[field.name] = field.value;  
         });
     employees.push(formData);
-    appendEmployee(formData);
+    appendEmployee();
     calcSalary();
     $('#employeeForm')[0].reset();
     return;
 }
 
-function appendEmployee (formData) {
-    
-     var el = '<tr>'+
-                '<td>'+formData.fName+'</td>'+
-                '<td>$'+formData.salary+'</td>'+
+function appendEmployee () {
+        var el2 =   '<tr>'+
+                        '<th>Employee Name</th>'+
+                        '<th>Salary</th>'+
+                        '<th>Remove</th>'+
+                    '</tr>';   
+                     
+   $('#employeeDisplay').empty();
+    $('#employeeDisplay').append(el2);
+     
+    for (var i = 0; i < employees.length; i++) {
+     var el = '<tr data-id="'+i+'">'+
+                '<td>'+employees[i].fName+'</td>'+
+                '<td>$'+employees[i].salary+'</td>'+
                 '<td><button class="btn btn-danger">Remove</button></td>'+
             +'</tr>';
-    
-    $('#employeeDisplay').append(el);
-    return;
-   
+        $('#employeeDisplay').append(el);     
+    }
+            
+
+    return;   
 }
 
 function calcSalary (){
@@ -33,6 +43,8 @@ function calcSalary (){
     for (var i = 0; i < employees.length; i++){
         total = total + parseInt(employees[i].salary);
     }
+    
+    
    
             $('#salary').empty();
             $('#salary').append('<h4>Total Salary</h4>'+
@@ -41,6 +53,8 @@ function calcSalary (){
     
     return;
 }
+
+
 
 $(document).ready(function(){
     
@@ -53,7 +67,10 @@ $(document).ready(function(){
     
     $('#employeeDisplay').on('click', 'button', function(){
        var el = $(this).parent().parent();
+       employees.splice($(el).attr('data-id'), 1);
+       console.log($(el).attr('data-id'));
        $(el).remove();
+       calcSalary();
     });
     
     
